e256ac203b1069d53dd9116ca24709c2bc2b6b0d
Besh Prakash Yogi <beshprakashktm@gmail.com>
2026-01-31 18:36:22 +0100

    __io_putchar created

--- 3_WorkingWithITM-Printf/Src/main.c
+++ 3_WorkingWithITM-Printf/Src/main.c
@@ -1,10 +1,16 @@
 # include "stm32f446xx.h"
 # include <string.h>
+# include <stdio.h>
 char _a[1] = {'A'};
 char *pt_a = _a;
 
 char hello[] = {"Hello from Besh"};
 char *p_hello = hello;
+
+int __io_putchar(int ch){
+	ITM_SendChar(ch);
+	return ch;
+}
 int main(void)
 {
 	for(int i = 0; i < strlen(hello); i++){
