30b8eba4b8f6317e1e078d16632b91e7903c6628
Besh Prakash Yogi <beshprakashktm@gmail.com>
2026-02-01 17:24:33 +0100

    workingwithUART created

---
+++ 5_WorkingWithUART/Src/main.c
@@ -0,0 +1,13 @@
+# include "swo.h"
+# include "stm32f446xx.h"
+
+int main(void)
+{
+	log_error("Initialization failed\n");
+	log_info("Filter complete\n");
+	log_debug("Warning, bad argument\n");
+	while(1)
+	{
+
+	}
+}
